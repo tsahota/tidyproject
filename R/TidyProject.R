f522c27675fd0d90a48d167f0e8ece783cd280ed
Tarjinder Sahota <klgk669@semldxzilker.seml.astrazeneca.net>
2017-02-22 15:11:11 +0100

    removed packrat

--- R/TidyProject.R
+++ R/TidyProject.R
@@ -540,86 +540,87 @@ replace_code_library <- function(path){
   options("code_library_path"=unique(path))
 }
 
-
-# Renvironment_info <- function(){
-#
-#   scripts.dir <- getOption("scripts.dir")
-#   scripts <- ls_scripts(scripts.dir)
-#   scripts <- scripts[!grepl("RenvironmentInfo\\.R$",scripts)]
-#
-#   text <- lapply(scripts,readLines)
-#   text <- unlist(text)
-#
-#   lib_statements1 <- text[grep("\\blibrary\\([^)]+\\)",text)]
-#   lib_statements1 <- gsub(".*(library\\([^)]+\\)).*","\\1",lib_statements1)
-#   ## remove reasons why lib_statements1 may be different (e.g. spaces)
-#   lib_statements1 <- gsub("\\s","",lib_statements1)
-#   lib_statements1 <- gsub("\\\\","",lib_statements1) ## no statement should have \\
-#   lib_statements1 <- unique(lib_statements1)
-#
-#   lib_statements2 <- text[grep("\\brequire\\([^)]+\\)",text)]
-#   lib_statements2 <- gsub(".*(require\\([^)]+\\)).*","\\1",lib_statements2)
-#   ## remove reasons why lib_statements2 may be different (e.g. spaces)
-#   lib_statements2 <- gsub("\\s","",lib_statements2)
-#   lib_statements2 <- gsub("\\\\","",lib_statements2) ## no statement should have \\
-#   lib_statements2 <- unique(lib_statements2)
-#
-#   lib_statements3 <- text[grep("\\w+\\s*::",text)]
-#   lib_statements3 <- gsub("^.*\\b(\\w+)\\s*::.*$","\\1",lib_statements3)
-#   lib_statements3 <- gsub("\\s","",lib_statements3)
-#   lib_statements3 <- unique(lib_statements3)
-#   if(length(lib_statements3)>0) lib_statements3 <- paste0("library(",lib_statements3,")")
-#
-#   lib_statements <- c(lib_statements1,lib_statements2,lib_statements3)
-#
-#   script <- c(
-#     "## Sourcing this script generate a sessionInfo for project",
-#     "## It will load all packages referenced in the scripts library",
-#     "## ",
-#     "",
-#     "## Load all packages ",
-#     lib_statements,
-#     "",
-#     "txt <- c(paste0(\"Created at \",Sys.time(),\" by \",Sys.info()[\"user\"],\"\\n\"))",
-#     "",
-#     "###### devtools::session_info output ######",
-#     "#txt <- c(txt,\"###### devtools::session_info output ######\")",
-#     "#x <- devtools::session_info(include_base = TRUE)",
-#     "#x$platform$running <- sessionInfo()$running",
-#     "#txt <- c(txt,capture.output(print(x)))",
-#     "",
-#     "###### utils::sessionInfo output ######",
-#     "txt <- c(txt,capture.output(sessionInfo()))",
-#     "",
-#     "writeLines(txt, \"RenvironmentInfo.txt\")"
-#   )
-#
-#   write(script,file=file.path(scripts.dir,"RenvironmentInfo.R"))
-#   suppressMessages(setup_file(file.path(scripts.dir,"RenvironmentInfo.R")))
-#   message(paste0("Script produced:           ",file.path(getOption("scripts.dir"),"RenvironmentInfo.R")))
-#   source(file.path(getOption("scripts.dir"),"RenvironmentInfo.R"))
-#   message(paste0("Environment info produced: RenvironmentInfo.txt"))
-#
-# }
-
 #' Make R session record
 #'
 #' Create a record of the R version and package versions used in a particular NMproject
 #'
 #' @export
+
 Renvironment_info <- function(){
-  #if(!requireNamespace("packrat", quietly = TRUE)) stop("function requires packrat to be installed")
 
-  dependent_packages <- dirDependencies(".")
-  sessionInfo(package = dependent_packages)
+  scripts.dir <- getOption("scripts.dir")
+  scripts <- ls_scripts(scripts.dir)
+  scripts <- scripts[!grepl("RenvironmentInfo\\.R$",scripts)]
 
-  txt <- c(paste0("Created at ",Sys.time()," by ",Sys.info()["user"],"\n"))
-  txt <- c(txt,capture.output(sessionInfo()))
-  writeLines(txt, "Renvironment_info.txt")
+  text <- lapply(scripts,readLines)
+  text <- unlist(text)
 
-  message(paste0("Environment info produced: Renvironment_info.txt"))
+  lib_statements1 <- text[grep("\\blibrary\\([^)]+\\)",text)]
+  lib_statements1 <- gsub(".*(library\\([^)]+\\)).*","\\1",lib_statements1)
+  ## remove reasons why lib_statements1 may be different (e.g. spaces)
+  lib_statements1 <- gsub("\\s","",lib_statements1)
+  lib_statements1 <- gsub("\\\\","",lib_statements1) ## no statement should have \\
+  lib_statements1 <- unique(lib_statements1)
+
+  lib_statements2 <- text[grep("\\brequire\\([^)]+\\)",text)]
+  lib_statements2 <- gsub(".*(require\\([^)]+\\)).*","\\1",lib_statements2)
+  ## remove reasons why lib_statements2 may be different (e.g. spaces)
+  lib_statements2 <- gsub("\\s","",lib_statements2)
+  lib_statements2 <- gsub("\\\\","",lib_statements2) ## no statement should have \\
+  lib_statements2 <- unique(lib_statements2)
+
+  lib_statements3 <- text[grep("\\w+\\s*::",text)]
+  lib_statements3 <- gsub("^.*\\b(\\w+)\\s*::.*$","\\1",lib_statements3)
+  lib_statements3 <- gsub("\\s","",lib_statements3)
+  lib_statements3 <- unique(lib_statements3)
+  if(length(lib_statements3)>0) lib_statements3 <- paste0("library(",lib_statements3,")")
+
+  lib_statements <- c(lib_statements1,lib_statements2,lib_statements3)
+
+  script <- c(
+    "## Sourcing this script generate a sessionInfo for project",
+    "## It will load all packages referenced in the scripts library",
+    "## ",
+    "",
+    "## Load all packages ",
+    lib_statements,
+    "",
+    "txt <- c(paste0(\"Created at \",Sys.time(),\" by \",Sys.info()[\"user\"],\"\\n\"))",
+    "",
+    "###### devtools::session_info output ######",
+    "#txt <- c(txt,\"###### devtools::session_info output ######\")",
+    "#x <- devtools::session_info(include_base = TRUE)",
+    "#x$platform$running <- sessionInfo()$running",
+    "#txt <- c(txt,capture.output(print(x)))",
+    "",
+    "###### utils::sessionInfo output ######",
+    "txt <- c(txt,capture.output(sessionInfo()))",
+    "",
+    "writeLines(txt, \"RenvironmentInfo.txt\")"
+  )
+
+  write(script,file=file.path(scripts.dir,"RenvironmentInfo.R"))
+  suppressMessages(setup_file(file.path(scripts.dir,"RenvironmentInfo.R")))
+  message(paste0("Script produced:           ",file.path(getOption("scripts.dir"),"RenvironmentInfo.R")))
+  source(file.path(getOption("scripts.dir"),"RenvironmentInfo.R"))
+  message(paste0("Environment info produced: RenvironmentInfo.txt"))
 
 }
+
+
+# Renvironment_info <- function(){
+#   #if(!requireNamespace("packrat", quietly = TRUE)) stop("function requires packrat to be installed")
+# 
+#   dependent_packages <- dirDependencies(".")
+#   sessionInfo(package = dependent_packages)
+# 
+#   txt <- c(paste0("Created at ",Sys.time()," by ",Sys.info()["user"],"\n"))
+#   txt <- c(txt,capture.output(sessionInfo()))
+#   writeLines(txt, "Renvironment_info.txt")
+# 
+#   message(paste0("Environment info produced: Renvironment_info.txt"))
+# 
+# }
 
 
 #' shorten path name
