c456483db81a2d9ea8a22e2496b6d50ef69d42f8
klgk669 <klgk669@rstudio-dev01.scp.astrazeneca.net>
2018-08-26 09:14:58 +0000

    changed order of Rprofile.R

--- inst/extdata/EmptyProject/Rprofile.R
+++ inst/extdata/EmptyProject/Rprofile.R
@@ -1,7 +1,8 @@
 ### Begin tidyproject config
 
+.remove_user_lib <- FALSE  ## if changing this, restart R for change to take effect
+
 if (file.exists(file.path("~", ".Rprofile"))) source(file.path("~", ".Rprofile"))
-.remove_user_lib <- FALSE
 if (file.exists("ProjectLibrary")) {
   if (length(.Library.site) == 0 | !.remove_user_lib) {
     .libPaths(c("ProjectLibrary", .libPaths()))
