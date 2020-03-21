8828e7dc3fa4aded9123b5e75bba88a122b65d2d
klgk669 <klgk669@rstudio-ha01.scp.astrazeneca.net>
2020-03-21 10:08:32 +0000

    proj_name returned by make_project

--- R/make_project.R
+++ R/make_project.R
@@ -158,7 +158,7 @@ make_project <- function(proj_name, lib_strategy = c("project-user","project","u
     message(paste("1. Open Rstudio project to start working: ", proj_name))
     message(paste("2. (optional) Install tidyproject package in project library"))
     
-    invisible(proj_name)
+    proj_name
 
 }
 
